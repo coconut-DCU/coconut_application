37adf80f9dd2034e22a951062e34ebf2c70ea2de
tjalsgur0083 <tjalsgur0083@gmail.com>
2023-10-19 17:11:59 +0900

    adding getX_Obx

--- forntEnd/lib/page/select_page.dart
+++ forntEnd/lib/page/select_page.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
-
-  // XFile? image;
-  // final ImagePicker picker = ImagePicker();
-
-  // Future getImage(ImageSource imageSource) async{
-  //   final XFile? pickedFile = await picker.pickImage(source: imageSource);
-  //   if(pickedFile != null) {
-  //     Obx((){
-  //       image = XFile(pickedFile.path);
-  //       return 
-  //     });
-  //   }
-  // }
+// final picker = ImagePicker();
+// XFile? image; 
+// List<XFile?> multiImage = []; 
+// List<XFile?> images = []; 
 
 class SelectPage extends StatelessWidget {
   const SelectPage({super.key});
@@ -23,7 +15,7 @@ class SelectPage extends StatelessWidget {
   Widget build(BuildContext context) {
     
     final cameraButton = Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           shape: RoundedRectangleBorder(
@@ -38,10 +30,31 @@ class SelectPage extends StatelessWidget {
         child: const Text('카메라 열기', style: TextStyle(color: Colors.black)),
       ),
       );
+    // Container(
+    //   margin:  EdgeInsets.all(10),
+    //   padding: EdgeInsets.all(5),
+    //   decoration: BoxDecoration(
+    //     color: Colors.lightBlueAccent,
+    //     borderRadius: BorderRadius.circular(24),
+    //     boxShadow: [
+    //       BoxShadow(
+    //         color: Colors.grey.withOpacity(0.5),
+    //         spreadRadius: 0.5,
+    //         blurRadius: 5
+    //       )
+    //     ],
+    //   ),
+    //   child: IconButton(
+    //     onPressed: () async {
+
+    //     },
+    //     icon: Icon(Icon.),
+    //   ),
+    // );
 
 
       final imageButton= Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           shape: RoundedRectangleBorder(
@@ -67,9 +80,9 @@ class SelectPage extends StatelessWidget {
             child: Center(child: null),
           ),
           cameraButton,
-          const Padding(padding: EdgeInsets.only(bottom: 300)),
+          const Padding(padding: EdgeInsets.only(bottom: 5)),
           imageButton,
-          const Padding(padding: EdgeInsets.only(bottom: 80)),
+          const Padding(padding: EdgeInsets.only(bottom: 90)),
         ],
       ),
     );
