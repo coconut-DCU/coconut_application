bdf43ad1b9b9efa76cb747965945c4ce39bd0014
MuchanKim <mutopia82@gmail.com>
2023-11-01 17:18:02 +0900

    update BE 11.1

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
-from src import api
+from src import routes
 
 app = FastAPI()
-app.include_router(api.router)
+app.include_router(routes.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/src/api/__init__.py
+++
@@ -1,2 +0,0 @@
-from fastapi import APIRouter
-from backend.src.api import img
